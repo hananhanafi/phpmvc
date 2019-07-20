2e481958f6a40f9aecc77c417c3045f181896dfb
Hanafih <hananhanafi@mail.ugm.ac.id>
2019-07-20 17:02:48 +0700

    .htacces pretty url

--- app/core/App.php
+++ app/core/App.php
@@ -1,8 +1,21 @@
 <?php
 class App {
-    public function __construct()
-    {
-        echo "Berhasil";
+    public function __construct(){
+        $url = $this->parseURL();
+        var_dump(($url));
+    }
+
+    //mengambil url dan memecah
+    public function parseURL(){
+        if(isset($_GET['url'])){
+            //menghapus / di depan
+            $url = rtrim($_GET['url'],'/');
+            //filter karakter aneh di url
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            //memecah url berdasarkan /
+            $url = explode('/',$url);
+            return $url;
+        }
     }
 }
 ?>
