72466b93215023c995954f8eadbc654cac0a9e13
Hanafih <hananhanafi@mail.ugm.ac.id>
2019-07-20 17:34:19 +0700

    Controller Method Params

---
+++ app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php
+class About{
+    public function index($nama = 'Hanan Hanafi',$pekerjaan = "Mahasiswa"){
+        echo "Halo saya $nama, saya adalah $pekerjaan";
+    }
+    public function page(){
+        echo 'About/page';
+    }
+}
+
+?>
--- app/core/App.php
+++ app/core/App.php
@@ -1,8 +1,33 @@
 <?php
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct(){
         $url = $this->parseURL();
-        var_dump(($url));
+        //controller
+        if(file_exists('../app/controllers/' . $url[0] . '.php')){
+            $this->controller = $url[0];
+            unset($url[0]);
+
+        }
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+        //method
+        if( isset($url[1]) ){
+            if(method_exists($this->method,$url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        } 
+        //params
+        if(!empty($url)){
+            $this->params = array_values($url);
+        }
+
+        //jalankan controller & method, serta params jika ada
+        call_user_func_array([$this->controller,$this->method],$this->params);
     }
 
     //mengambil url dan memecah
